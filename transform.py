dd6bd18f40077016d298088bda7a8906dc224c71
Gabriel <gabrielnsiah2011@gmail.com>
2024-11-26 17:06:33 +0000

    Completed transform.py

--- transform.py
+++ transform.py
@@ -4,22 +4,54 @@ import pandas as pd
 
 def insert_in_dataframe(plant_data: list[dict]) -> pd.DataFrame:
     """Creates a dataframe for plant data
-        Inserts plant data for each plant inside
-        Returns the dataframe."""
+       Inserts plant data for each plant inside
+       Returns the dataframe."""
 
-    return plant_data
+    plant_df = pd.json_normalize(plant_data)
 
+    location_df = plant_df['origin_location'].apply(pd.Series)
 
-def clean_data(plant_df: pd.DataFrame):
-    """Cleans dataframe of erroneous/extreme values"""
+    location_df.columns = ['Latitude', 'Longitude',
+                           'Town', 'Country_Code', 'Continent_City']
+
+    location_df[['Continent', 'City']
+                ] = location_df['Continent_City'].str.split('/', expand=True)
+
+    location_df = location_df.drop(columns=['Continent_City'])
+
+    plant_df = plant_df.drop(columns=['origin_location'])
+
+    plant_df = pd.concat([plant_df, location_df], axis=1)
+
+    plant_df['scientific_name'] = plant_df['scientific_name'].apply(
+        lambda x: x[0] if isinstance(x, list) else x)
+
+    plant_df[["First Name", "Last Name"]
+             ] = plant_df["botanist.name"].str.split(" ", n=1, expand=True)
 
     return plant_df
 
 
-def export_as_csv(plant_df: pd.DataFrame):
+def clean_data(plant_df: pd.DataFrame) -> pd.DataFrame:
+    """Cleans dataframe of unnecessary data
+        Removes unwanted columns"""
+
+    plant_df["City"] = plant_df["City"].str.replace("_", " ")
+
+    columns_to_drop = ["images.license", "images.license_name", "images.license_url",
+                       "images.medium_url", "images.small_url", "images.thumbnail",
+                       "images.regular_url", "botanist.name"]
+
+    plant_df = plant_df.where(pd.notnull(plant_df), "None")
+
+    plant_df = plant_df.drop(columns=columns_to_drop)
+
+    return plant_df
+
+
+def export_as_csv(plant_df: pd.DataFrame) -> None:
     """Exports the dataframe as a csv file."""
-
-    return plant_df
+    plant_df.to_csv("PLANT_DATA.csv", index=False)
 
 
 if __name__ == "__main__":
