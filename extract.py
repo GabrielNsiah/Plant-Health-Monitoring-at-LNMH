9faa657b2456f1f8a98a1162da8bc659a1b934e1
Luke <lukieh2014@gmail.com>
2024-11-26 13:11:40 +0000

    Pylinted function headings

--- extract.py
+++ extract.py
@@ -10,17 +10,17 @@ def function_to_test():
 def fetch_api_plant_data(plant_id: int) -> dict:
     """Gets plant data for a plant with given id, 
     returning the data as a dictionary."""
-    pass
+    ...
 
 
 def get_all_plant_data() -> list[dict]:
     """Returns all plant data for all 50 plants as
     a list of dictionaries."""
-    pass
+    ...
 
 
 if __name__ == "__main__":
     response = requests.get(
-        "https://data-eng-plants-api.herokuapp.com/plants/1")
+        "https://data-eng-plants-api.herokuapp.com/plants/1", timeout=100)
 
     print(response.json())
