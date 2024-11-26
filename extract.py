48cceaa7e4ae3025e155adf06a75e1129ab9e77c
Gabriel <50891625+GabrielNsiah@users.noreply.github.com>
2024-11-26 13:32:34 +0000

    Revert "16 extract script"

--- extract.py
+++ extract.py
@@ -8,24 +8,19 @@ def function_to_test():
 
 
 def fetch_api_plant_data(plant_id: int) -> dict:
-    """Gets plant data for a plant with given id,
+    """Gets plant data for a plant with given id, 
     returning the data as a dictionary."""
-    response = requests.get(
-        f"https://data-eng-plants-api.herokuapp.com/plants/{str(plant_id)}", timeout=100)
-    return response.json()
+    return
 
 
 def get_all_plant_data() -> list[dict]:
     """Returns all plant data for all 50 plants as
     a list of dictionaries."""
-    data_list = []
-    for i in range(0, 51):
-        plant_data = fetch_api_plant_data(i)
-        if "error" in plant_data.keys():
-            continue
-        data_list.append(plant_data)
-    return data_list
+    return
 
 
 if __name__ == "__main__":
-    pass
+    response = requests.get(
+        "https://data-eng-plants-api.herokuapp.com/plants/1", timeout=100)
+
+    print(response.json())
