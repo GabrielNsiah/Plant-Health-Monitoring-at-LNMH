3e28f684921effc473310606075002a9a78dc032
Luke <lukieh2014@gmail.com>
2024-11-26 18:02:07 +0000

    finished making schema.sql to create empty tables and seed continents table

---
+++ schema.sql
@@ -0,0 +1,91 @@
+
+IF OBJECT_ID('delta.Recordings', 'U') IS NOT NULL
+    DROP TABLE delta.Recordings;
+
+IF OBJECT_ID('delta.Assignments', 'U') IS NOT NULL
+    DROP TABLE delta.Assignments;
+
+IF OBJECT_ID('delta.Plants', 'U') IS NOT NULL
+    DROP TABLE delta.Plants;
+
+IF OBJECT_ID('delta.Locations', 'U') IS NOT NULL
+    DROP TABLE delta.Locations;
+
+IF OBJECT_ID('delta.Continents', 'U') IS NOT NULL
+    DROP TABLE delta.Continents;
+
+IF OBJECT_ID('delta.Botanists', 'U') IS NOT NULL
+    DROP TABLE delta.Botanists;
+
+IF OBJECT_ID('delta.Scientific_Names', 'U') IS NOT NULL
+    DROP TABLE delta.Scientific_Names;
+
+
+CREATE TABLE delta.Continents (
+    continent_id INT NOT NULL UNIQUE,
+    continent_name VARCHAR(30) NOT NULL UNIQUE,
+    primary key (continent_id)
+);
+
+INSERT INTO delta.Continents (continent_id, continent_name) VALUES
+(1, 'North America'),
+(2, 'South America'),
+(3, 'Asia'),
+(4, 'Antarctica'),
+(5, 'Europe'),
+(6, 'Africa'),
+(7, 'Oceania')
+;
+
+CREATE TABLE delta.Locations (
+    location_id INT IDENTITY (1, 1) PRIMARY KEY,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    continent_id INT NOT NULL,
+    country_code VARCHAR(2) NOT NULL,
+    city VARCHAR(25),
+    town VARCHAR(25),
+    FOREIGN KEY (continent_id) REFERENCES delta.Continents (continent_id)
+);
+
+CREATE TABLE delta.Scientific_Names (
+    scientific_id INT IDENTITY (1, 1) PRIMARY KEY,
+    scientific_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE delta.Botanists (
+    botanist_id INT IDENTITY (1, 1) PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    phone VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE delta.Plants (
+    plant_id INT UNIQUE NOT NULL,
+    plant_name VARCHAR(30) UNIQUE,
+    scientific_id INT UNIQUE,
+    location_id INT NOT NULL,
+    image_url VARCHAR(100),
+    primary key (plant_id),
+    FOREIGN KEY (scientific_id) REFERENCES delta.Scientific_Names (scientific_id),
+    FOREIGN KEY (location_id) REFERENCES delta.Locations (location_id)
+);
+
+CREATE TABLE delta.Recordings (
+    recording_id INT IDENTITY (1, 1) PRIMARY KEY,
+    plant_id INT NOT NULL,
+    last_watered datetime2,
+    soil_moisture FLOAT NOT NULL,
+    temperature FLOAT NOT NULL,
+    reading_taken datetime2 NOT NULL,
+    FOREIGN KEY (plant_id) REFERENCES delta.Plants (plant_id)
+);
+
+CREATE TABLE delta.Assignments (
+    assignment_id INT IDENTITY (1, 1) PRIMARY KEY,
+    botanist_id INT NOT NULL,
+    plant_id INT NOT NULL,
+    FOREIGN KEY (botanist_id) REFERENCES delta.Botanists (botanist_id),
+    FOREIGN KEY (plant_id) REFERENCES delta.Plants (plant_id)
+);
