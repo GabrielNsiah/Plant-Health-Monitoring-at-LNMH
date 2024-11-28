4822490af384a988aae8a1f50c1c921df8889721
Gabriel <gabrielnsiah2011@gmail.com>
2024-11-28 17:41:00 +0000

    Made a start on the dashboard

---
+++ streamlit/dashboard.py
@@ -0,0 +1,28 @@
+"""File for the streamlit dashboard"""
+
+import streamlit as st
+import pandas as pd
+import redshift_connector
+import pymssql
+import os
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection() -> None:
+    """Returns a connection to the database"""
+    connection = pymssql.connect(
+        server=environ["DB_HOST"],
+        port=environ["DB_PORT"],
+        user=environ["DB_USER"],
+        password=environ["DB_PASSWORD"],
+        database=environ["DB_NAME"]
+    )
+
+    return connection
+
+
+if __name__ == "__main__":
+    pass
