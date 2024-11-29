8bcfe8da7b604f83e6e9c0863073be6526737e1c
RidwanHamid501 <ridwanhamid501@gmail.com>
2024-11-29 17:13:16 +0000

    Dockerise dashboard

---
+++ terraform/ECS_terraform/var.tf
@@ -0,0 +1,68 @@
+variable "DB_HOST" {
+    type = string
+}
+
+variable "DB_PORT" {
+    type = string
+}
+
+variable "DB_PASSWORD" {
+    type = string
+}
+
+variable "DB_USER" {
+    type = string
+}
+
+variable "DB_NAME" {
+    type = string
+}
+
+variable "SCHEMA_NAME" {
+    type = string
+}
+
+variable "URI" {
+    type = string
+}
+
+variable "FAMILY" {
+    type = string
+}
+
+variable "ROLE_ARN" {
+    type = string
+}
+
+variable "CONTAINER" {
+    type = string
+}
+
+variable "C14_CLUSTER" {
+    type = string
+}
+
+variable "C14_VPC" {
+    type = string
+}
+
+variable "C14_SUBNET_1" {
+    type = string
+}
+
+variable "C14_SUBNET_2" {
+    type = string
+}
+
+variable "C14_SUBNET_3" {
+    type = string
+}
+
+variable "aws_access_key_id" {
+    type = string
+}
+
+variable "aws_secret_access_key" {
+    type = string
+}
+
